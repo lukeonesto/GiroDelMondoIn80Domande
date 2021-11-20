a1f68b9ee54adf8efbae8e13878e702c6c84918b
Luke <amato.luca.rock@gmail.com>
2021-11-20 18:12:45 +0100

    aggiunta la HomeActivity.kt

--- app/src/main/java/com/example/girodelmondoin80domande/MainActivity.kt
+++ app/src/main/java/com/example/girodelmondoin80domande/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.girodelmondoin80domande
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startHomeAfter()
+    }
+
+    /**
+     *  Lancia la Home dopo aver aspettato il tempo della animazione.
+     */
+    private fun startHomeAfter() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
